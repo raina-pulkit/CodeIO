8fbf32e72e126e1d26c33dac8e390eff38b82e24
Pulkit Raina <pulkitraina2608@gmail.com>
2024-07-09 11:43:05 +0530

    modified:   client/src/pages/u/teacherDashboard/teacherDashboard.tsx

--- client/src/pages/u/teacherDashboard/teacherDashboard.tsx
+++ client/src/pages/u/teacherDashboard/teacherDashboard.tsx
@@ -2,20 +2,18 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
-
 const CourseCode = ({ code, name }: { code: string; name: string }) => {
 	const navigate = useNavigate();
 
-    return (
-    <div
-      className="mb-4 py-5 px-10 bg-slate-600 rounded-lg hover:scale-95 active:scale-90 transition-all duration-200 cursor-pointer flex flex-col justify-center items-center"
-      onClick={() => navigate(`/u/${code}`)}
-    >
-      <span>{code}</span>
-      <span>{name}</span>
-    </div>
-  );
+	return (
+		<div
+			className="mb-4 py-5 px-10 bg-slate-600 rounded-lg hover:scale-95 active:scale-90 transition-all duration-200 cursor-pointer flex flex-col justify-center items-center"
+			onClick={() => navigate(`/u/${code}`)}
+		>
+			<span>{code}</span>
+			<span>{name}</span>
+		</div>
+	);
 };
 
 const TeacherDashboard = () => {
@@ -24,32 +22,38 @@ const TeacherDashboard = () => {
 
 	useEffect(() => {
 		const fun = async () => {
-		  const response = await axios.get(`api/courses`, {
-			headers: {
-			  authorization: localStorage.getItem("token"),
-			},
-		  });
-		  setCodes(response.data.courseCodes);
-		  setNames(response.data.courseNames);
+			const response = await axios.get(`api/courses`, {
+				headers: {
+					authorization: localStorage.getItem("token"),
+				},
+			});
+			setCodes(response.data.courseCodes);
+			setNames(response.data.courseNames);
 		};
 		fun();
-	  }, []);
-  return (
-    <div className="flex flex-wrap py-10 px-14 md:px-40 gap-10 justify-center items-center">
-      {codes &&
-        names &&
-        codes.map((item: string, index: number) => {
-          return <CourseCode code={item} name={names[index]} key={index}></CourseCode>;
-        })}
-      {/* <CourseCode />
+	}, []);
+	return (
+		<div className="flex flex-wrap py-10 px-14 md:px-40 gap-10 justify-center items-center">
+			{codes &&
+				names &&
+				codes.map((item: string, index: number) => {
+					return (
+						<CourseCode
+							code={item}
+							name={names[index]}
+							key={index}
+						></CourseCode>
+					);
+				})}
+			{/* <CourseCode />
 			<CourseCode />
 			<CourseCode />
 			<CourseCode />
 			<CourseCode /> 
 			<CourseCode />
 			<CourseCode /> */}
-    </div>
-  );
+		</div>
+	);
 };
 
 export default TeacherDashboard;
